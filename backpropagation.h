7734765082e4f09374107119155d46f330057905
York Jasper Niebuhr <yjnworkstation@gmail.com>
2022-12-24 02:49:04 +0100

    Implemented neural network functionality

--- backpropagation.h
+++ backpropagation.h
@@ -1,6 +1,12 @@
 #pragma once
 #include <functional>
 #include <cstddef>
+#include <random>
+
+template<typename T>
+T random(T min, T max) {
+    return (static_cast<T>(rand()) / static_cast<T>(RAND_MAX)) * (max - min) + min;
+}
 
 template<typename T, std::size_t M, std::size_t N>
 class Matrix {
@@ -14,28 +20,31 @@ public:
     Matrix(T v);
     Matrix(std::function<T(std::size_t, std::size_t)> f); 
     
+    // randomize
+    void randomize(T min, T max);
+
     // scalar multiplication
     Matrix<T, M, N>& operator*=(T rhs);
-    Matrix<T, M, N> operator*(T rhs);
+    Matrix<T, M, N> operator*(T rhs) const;
 
     // elementwise multiplication
     Matrix<T, M, N>& operator*=(const Matrix<T, M, N>& rhs);
 
     // scalar addition
     Matrix<T, M, N>& operator+=(T rhs);
-    Matrix<T, M, N> operator+(T rhs);
+    Matrix<T, M, N> operator+(T rhs) const;
 
     // matrix addition
     Matrix<T, M, N>& operator+=(const Matrix<T, M, N>& rhs);
-    Matrix<T, M, N> operator+(const Matrix<T, M, N>& rhs);
+    Matrix<T, M, N> operator+(const Matrix<T, M, N>& rhs) const;
     
     // matrix subtraction
     Matrix<T, M, N>& operator-=(const Matrix<T, M, N>& rhs);
-    Matrix<T, M, N> operator-(const Matrix<T, M, N>& rhs);
+    Matrix<T, M, N> operator-(const Matrix<T, M, N>& rhs) const;
 
     // map
     Matrix<T, M, N>& operator<<=(std::function<T(T)> f);
-    Matrix<T, M, N> operator<<(std::function<T(T)> f);
+    Matrix<T, M, N> operator<<(std::function<T(T)> f) const;
 
     // indexing
     T& operator()(std::size_t m, std::size_t n);
@@ -47,6 +56,65 @@ public:
     // matrix multiplication
     template<std::size_t J, std::size_t K>
     Matrix<T, M, K> operator*(const Matrix<T, J, K>& rhs);
+};
+
+template<typename T, T(*Activation)(T), T(*Derivative)(T), std::size_t I, std::size_t... L>
+class BPNet {
+private:
+    using SubNetType = BPNet<T, Activation, Derivative, L...>;
+public:
+    static constexpr T(*ACTIVATION)(T) = Activation;
+    static constexpr T(*DERIVATIVE)(T) = Derivative;
+    static constexpr std::size_t INPUTS = I;
+    static constexpr std::size_t OUTPUTS = SubNetType::OUTPUTS;
+    static constexpr T LEARNING_RATE = static_cast<T>(0.002);
+private:
+    static constexpr std::size_t NEXT = SubNetType::INPUTS;
+
+    Matrix<T, NEXT, INPUTS> m_weight;
+    Matrix<T, NEXT, 1> m_bias;
+    SubNetType m_sub;
+public:
+    void randomize(T min, T max) {
+        m_weight.randomize(min, max);
+	m_bias.randomize(min, max);
+	m_sub.randomize(min, max);
+    }
+
+    Matrix<T, OUTPUTS, 1> get(const Matrix<T, INPUTS, 1>& input) {
+	return m_sub.get(((m_weight * input) + m_bias) << ACTIVATION);
+    }
+
+    Matrix<T, INPUTS, 1> train(const Matrix<T, INPUTS, 1>& input, const Matrix<T, OUTPUTS, 1>& output) {
+        Matrix<T, NEXT, 1> temp = ((m_weight * input) + m_bias) << ACTIVATION;
+        Matrix<T, NEXT, 1> errors = m_sub.train(temp, output);
+        
+	Matrix<T, NEXT, 1> gradient = temp << DERIVATIVE;
+        gradient *= errors;
+	gradient *= LEARNING_RATE;
+	
+	Matrix<T, 1, INPUTS> hidden = ~input;
+	Matrix<T, NEXT, INPUTS> deltas = gradient * hidden;
+
+	m_weight += deltas;
+	m_bias += gradient;
+
+	return ~m_weight * errors;
+    }
+};
+
+template<typename T, T(*Activation)(T), T(*Derivative)(T), std::size_t O>
+class BPNet<T, Activation, Derivative, O> {
+public:
+    static constexpr T(*ACTIVATION)(T) = Activation;
+    static constexpr T(*DERIVATIVE)(T) = Derivative;
+    static constexpr std::size_t INPUTS = O;
+    static constexpr std::size_t OUTPUTS = O;
+    static constexpr T LEARNING_RATE = static_cast<T>(0.002);
+public:
+    void randomize(T min, T max) {}
+    Matrix<T, OUTPUTS, 1> get(const Matrix<T, INPUTS, 1>& input) { return input; } 
+    Matrix<T, INPUTS, 1> train(const Matrix<T, INPUTS, 1>& input, const Matrix<T, OUTPUTS, 1>& output) { return output - input; }
 };
 
 template<typename T, std::size_t M, std::size_t N>
@@ -68,6 +136,15 @@ Matrix<T, M, N>::Matrix(std::function<T(std::size_t, std::size_t)> f) {
 }
 
 template<typename T, std::size_t M, std::size_t N>
+void Matrix<T, M, N>::randomize(T min, T max) {
+    for (std::size_t m = 0; m < M; m++) {
+        for (std::size_t n = 0; n < N; n++) {
+            data[m][n] = random<T>(min, max);
+        }
+    }
+}
+
+template<typename T, std::size_t M, std::size_t N>
 Matrix<T, M, N>& Matrix<T, M, N>::operator*=(T rhs) {
     for (std::size_t m = 0; m < M; m++) {
         for (std::size_t n = 0; n < N; n++) {
@@ -78,7 +155,7 @@ Matrix<T, M, N>& Matrix<T, M, N>::operator*=(T rhs) {
 }
 
 template<typename T, std::size_t M, std::size_t N>
-Matrix<T, M, N> Matrix<T, M, N>::operator*(T rhs) {
+Matrix<T, M, N> Matrix<T, M, N>::operator*(T rhs) const {
     Matrix<T, M, N> result = *this;
     result *= rhs;
     return result;
@@ -105,7 +182,7 @@ Matrix<T, M, N>& Matrix<T, M, N>::operator+=(T rhs) {
 }
 
 template<typename T, std::size_t M, std::size_t N>
-Matrix<T, M, N> Matrix<T, M, N>::operator+(T rhs) {
+Matrix<T, M, N> Matrix<T, M, N>::operator+(T rhs) const {
     Matrix<T, M, N> result = *this;
     result += rhs;
     return result;
@@ -122,7 +199,7 @@ Matrix<T, M, N>& Matrix<T, M, N>::operator+=(const Matrix<T, M, N>& rhs) {
 }
 
 template<typename T, std::size_t M, std::size_t N>
-Matrix<T, M, N> Matrix<T, M, N>::operator+(const Matrix<T, M, N>& rhs) {
+Matrix<T, M, N> Matrix<T, M, N>::operator+(const Matrix<T, M, N>& rhs) const {
     Matrix<T, M, N> result = *this;
     result += rhs;
     return result;
@@ -139,7 +216,7 @@ Matrix<T, M, N>& Matrix<T, M, N>::operator-=(const Matrix<T, M, N>& rhs) {
 }
 
 template<typename T, std::size_t M, std::size_t N>
-Matrix<T, M, N> Matrix<T, M, N>::operator-(const Matrix<T, M, N>& rhs) {
+Matrix<T, M, N> Matrix<T, M, N>::operator-(const Matrix<T, M, N>& rhs) const {
     Matrix<T, M, N> result = *this;
     result -= rhs;
     return result;
@@ -156,7 +233,7 @@ Matrix<T, M, N>& Matrix<T, M, N>::operator<<=(std::function<T(T)> f) {
 }
 
 template<typename T, std::size_t M, std::size_t N>
-Matrix<T, M, N> Matrix<T, M, N>::operator<<(std::function<T(T)> f) {
+Matrix<T, M, N> Matrix<T, M, N>::operator<<(std::function<T(T)> f) const {
     Matrix<T, M, N> result = *this;
     result <<= f;
     return result;
@@ -198,11 +275,3 @@ Matrix<T, M, K> Matrix<T, M, N>::operator*(const Matrix<T, J, K>& rhs) {
     }
     return result;
 }
-
-/*
- * T = either float or double
- * A = activation function
- * D = derivative of activation function
- * L = layer sizes
- */
-//template<typename T, std::function<> A, std::function<> D, size_t... L>
--- examples/linear_graph.cpp
+++ examples/linear_graph.cpp
@@ -1,6 +1,10 @@
 #include "../backpropagation.h"
 #include <iostream>
 #include <sstream>
+#include <cmath>
+
+double sigmoid(double x) { return 1.0 / (1.0 + std::exp(-x)); }
+double dsigmoid(double x) { return x * (1.0 - x); }
 
 template <typename T>
 std::string to_string_with_precision(const T a_value, const int n = 6)
@@ -21,12 +25,81 @@ void printMatrix(const Matrix<T, M, N>& matrix) {
     }
 }
 
+// Linear graph definition
+double f(double x) {
+    return 0.42 * x;
+}
+
+// Oracle to tell wether a point is over the line or not
+bool oracle(double x, double y) {
+    return y > f(x);
+}
+
 int main() {
-    Matrix<float, 16, 8> m{ 0.f };
+    // Define neural network structure
+    using NetType = BPNet<double, sigmoid, dsigmoid, 2, 4, 1>;
+    static constexpr std::size_t TrainingCycles = 1000000;
+    static constexpr std::size_t ControlCycles = 1000;
 
-    printMatrix(m);
-    std::cout << std::endl;
-    printMatrix(~m);
+    // Set seed of random number generator
+    srand(static_cast<unsigned>(time(0)));
 
+    // Create neural network instance and randomize matrices
+    NetType net;
+    net.randomize(0.0, 1.0);
+
+    // Train network with data (known input-output relations)
+    for (std::size_t i = 0; i < TrainingCycles; i++) {
+        // Generate random inputs
+	Matrix<double, 2, 1> inputs;
+	inputs.randomize(0.0, 1.0);
+
+	// Wanted output (1 if over line, 0 if under line)
+	Matrix<double, 1, 1> outputs;
+	outputs(0, 0) = oracle(inputs(0, 0), inputs(1, 0)) ? 1.0 : 0.0;
+
+	// Train network and get input errors as result
+	Matrix<double, 2, 1> errors = net.train(inputs, outputs);
+    }
+
+    // Feed input data into the neural network and calculate success rate
+    std::size_t correct = 0;
+    std::size_t over = 0;
+    double average_error = 0.0;
+
+    for (std::size_t i = 0; i < ControlCycles; i++) {
+        // Generate random inputs
+	Matrix<double, 2, 1> inputs;
+        inputs.randomize(0.0, 1.0);
+
+        // Wanted output (1 if over line, 0 if under line)
+        double actual = oracle(inputs(0, 0), inputs(1, 0)) ? 1.0 : 0.0;
+        double wrong = !oracle(inputs(0, 0), inputs(1, 0)) ? 1.0 : 0.0;
+
+        // Count how many inputs are over the line
+	if (oracle(inputs(0, 0), inputs(1, 0))) {
+	    over++;
+	}
+
+	// Get network output
+	double output = net.get(inputs)(0, 0);
+        
+	// Error
+        average_error += std::abs(actual - output) / static_cast<double>(ControlCycles);
+
+	// Success if output was closer to correct result
+	if (std::abs(actual - output) < std::abs(wrong - output)) {
+	    correct++;
+	}
+    }
+
+    // Success calculations
+    double success_percentage = static_cast<double>(correct) / static_cast<double>(ControlCycles) * 100.0;
+    double over_rate = static_cast<double>(over) / static_cast<double>(ControlCycles) * 100.0;
+
+    // Output result
+    std::cout << "Of the " << ControlCycles << " control runs (" << over_rate << "% over the line) " << success_percentage << "% resulted in the correct output with an average error of " << average_error << "." << std::endl;
+
+    // End of program
     return 0;
 }
